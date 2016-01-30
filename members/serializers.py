8ea535aa9719387c05518f8ca3029bf9a51df12c
Magnus <magnus@superschnell.dk>
2016-01-30 15:43:52 +0100

    API: Department all properties in serializer

--- members/serializers.py
+++ members/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Department
-        fields = ('name', 'description', 'open_hours', 'responsible_name', 'responsible_contact')
+        fields = ('name', 'description', 'open_hours', 'responsible_name', 'responsible_contact','placename','zipcode','city','streetname','housenumber','floor','door','dawa_id','has_waiting_list')
